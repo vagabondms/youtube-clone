21a9c2c626a982f349e6f11b3c832e9b047c2d03
vagabondms <hiseokseok@gmail.com>
2022-01-18 00:20:29 +0900

    feat(core): init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-
-import "./App.css";
+import './App.css';
 
 function App() {
-  return <div> app</div>;
+  return <div>app</div>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
-import * as serviceWorker from "./serviceWorker";
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Provider } from 'react-redux';
+import App from './App';
+import { store } from './app/store';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>,
+  document.getElementById('root'),
 );
 
 serviceWorker.unregister();
